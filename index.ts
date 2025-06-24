642968468a543da0d6d7600ce533caa7c4210916
Sara Cristina <saracristinacs09@gmail.com>
2025-06-24 01:08:20 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-import Fastify from 'fastify';
-import userroutes from './routes/userroutes';
 
+import express from 'express';
+import categoryRoutes from './routes/categoryroutes.ts';
+import transactionRoutes from './routes/transactionsroutes.ts';
+import bankRoutes from './routes/bankroutes.ts';
 
+const app = express();
+app.use(express.json());
 
+// Rotas separadas
+app.use('/categories', categoryRoutes);
+app.use('/transactions', transactionRoutes);
+app.use('/banks', bankRoutes);
 
-const app = Fastify();
 const PORT = 3000;
-
-app.register(userroutes, { prefix: '/api/users' });
-
-app.listen({ port: PORT }, () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
